6b4169e566f6caf36e5cec3f5d2d86de93c12c80
Sourish16 <sssourish4@gmail.com>
2022-07-24 18:51:43 +0530

    Add Cart Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Badge from "react-bootstrap/Badge";
-import Container from "react-bootstrap/Container";
-import { LinkContainer } from "react-router-bootstrap";
-import { useContext } from "react";
-import { Store } from "./Store";
-import { Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Badge from 'react-bootstrap/Badge';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useContext } from 'react';
+import { Store } from './Store';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   const { state } = useContext(Store);
   const { cart } = state;
   return (
     <BrowserRouter>
-      <div className='d-flex flex-column site-container'>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Navbar bg='dark' variant='dark'>
+          <Navbar bg="dark" variant="dark">
             <Container>
-              <LinkContainer to='/'>
+              <LinkContainer to="/">
                 <Navbar.Brand>amazona</Navbar.Brand>
               </LinkContainer>
-              <Nav className='me-auto'>
-                <Link to='/cart' className='nav-link'>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
                   Cart
                   {cart.cartItems.length > 0 && (
-                    <Badge pill bg='danger'>
+                    <Badge pill bg="danger">
                       {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </Badge>
                   )}
@@ -36,15 +36,16 @@ function App() {
           </Navbar>
         </header>
         <main>
-          <Container className='mt-3'>
+          <Container className="mt-3">
             <Routes>
-              <Route path='/' element={<HomeScreen />} />
-              <Route path='/product/:slug' element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
             </Routes>
           </Container>
         </main>
         <footer>
-          <div className='text-center'>All rights reserved.</div>
+          <div className="text-center">All rights reserved.</div>
         </footer>
       </div>
     </BrowserRouter>
