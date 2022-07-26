c0e324fffd491d1a3af8505f028d37cfbbd8845b
Sourish16 <sssourish4@gmail.com>
2022-07-26 18:25:37 +0530

    Create-Signin backend API

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -17,9 +18,19 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //form data in the post request is converted to json object in req.body
+
 app.use('/api/seed', seedRouter);
 
 app.use('/api/products', productRouter);
+
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
